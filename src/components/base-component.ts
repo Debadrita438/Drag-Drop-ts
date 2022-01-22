bb6dddda310544f3fd641092491238887502cc68
Debadrita438 <bristibandyopadhyay@gmail.com>
2022-01-22 15:47:59 +0530

    Seperated files using namspace

---
+++ src/components/base-component.ts
@@ -0,0 +1,37 @@
+namespace App {
+    // Component Base Class
+    export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+        templateElement: HTMLTemplateElement;
+        hostElement: T;
+        element: U;
+
+        constructor(
+            templateId: string,
+            hostElementId: string,
+            inserAtStart: boolean,
+            newElementId?: string
+        ) {
+            this.templateElement = <HTMLTemplateElement>document.getElementById(templateId)!;
+            this.hostElement = <T>document.getElementById(hostElementId)!;
+
+            const importedNode = document.importNode(this.templateElement.content, true);
+
+            // Since importedNode is inside constructor, so it can't be directly accessed. It can't be directly used either inside insertAdjacentElement because it's of type fragment
+            // So we have to do it like below
+            this.element = <U>importedNode.firstElementChild;
+            if (newElementId) {
+                this.element.id = newElementId;
+            }
+            this.attach(inserAtStart);
+        }
+        private attach(insertAtBeginning: boolean) {
+            this.hostElement.insertAdjacentElement(
+                insertAtBeginning ? 'afterbegin' : 'beforeend',
+                this.element
+            );
+        }
+
+        abstract configure(): void;
+        abstract renderContent(): void;
+    }
+}
