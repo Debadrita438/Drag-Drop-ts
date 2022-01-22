aec900239a09808ff0bc4139b16336a5c76ce89f
Debadrita438 <bristibandyopadhyay@gmail.com>
2022-01-22 17:00:23 +0530

    implementing es6 module to seperate files

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,79 +1,77 @@
-///<reference path="base-component.ts" />
-///<reference path="../decorators/autobind.ts" />
-///<reference path="../util/validation.ts" />
-///<reference path="../state/project-state.ts" />
-namespace App {
-    // Project Input class
-    export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
-        titleInputElement: HTMLInputElement;
-        descriptionInputElement: HTMLInputElement;
-        peopleInputElement: HTMLInputElement;
+// Project Input class
+import Component from './base-component.js';
+import * as Validation from '../util/validation.js';
+import { Autobind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
-        constructor() {
-            super('project-input', 'app', true, 'user-input');
+    constructor() {
+        super('project-input', 'app', true, 'user-input');
 
-            this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')!;
-            this.descriptionInputElement = <HTMLInputElement>(
-                this.element.querySelector('#description')!
-            );
-            this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')!;
+        this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')!;
+        this.descriptionInputElement = <HTMLInputElement>(
+            this.element.querySelector('#description')!
+        );
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')!;
 
-            this.configure();
+        this.configure();
+    }
+
+    renderContent(): void {}
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        const titleValidatable: Validation.Validatable = {
+            value: enteredTitle,
+            required: true,
+        };
+        const descriptionValidatable: Validation.Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+        };
+        const peopleValidatable: Validation.Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+
+        if (
+            !Validation.validate(titleValidatable) ||
+            !Validation.validate(descriptionValidatable) ||
+            !Validation.validate(peopleValidatable)
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
         }
+    }
 
-        renderContent(): void {}
-        configure() {
-            this.element.addEventListener('submit', this.submitHandler);
-        }
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
 
-        private gatherUserInput(): [string, string, number] | void {
-            const enteredTitle = this.titleInputElement.value;
-            const enteredDescription = this.descriptionInputElement.value;
-            const enteredPeople = this.peopleInputElement.value;
-
-            const titleValidatable: Validatable = {
-                value: enteredTitle,
-                required: true,
-            };
-            const descriptionValidatable: Validatable = {
-                value: enteredDescription,
-                required: true,
-                minLength: 5,
-            };
-            const peopleValidatable: Validatable = {
-                value: +enteredPeople,
-                required: true,
-                min: 1,
-                max: 5,
-            };
-
-            if (
-                !validate(titleValidatable) ||
-                !validate(descriptionValidatable) ||
-                !validate(peopleValidatable)
-            ) {
-                alert('Invalid input, please try again!');
-                return;
-            } else {
-                return [enteredTitle, enteredDescription, +enteredPeople];
-            }
-        }
-
-        private clearInputs() {
-            this.titleInputElement.value = '';
-            this.descriptionInputElement.value = '';
-            this.peopleInputElement.value = '';
-        }
-
-        @Autobind
-        private submitHandler(event: Event) {
-            event.preventDefault();
-            const userInput = this.gatherUserInput();
-            if (Array.isArray(userInput)) {
-                const [title, desc, people] = userInput;
-                projectState.addProject(title, desc, people);
-                this.clearInputs();
-            }
+    @Autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            projectState.addProject(title, desc, people);
+            this.clearInputs();
         }
     }
 }
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,50 +1,46 @@
-///<reference path="base-component.ts" />
-///<reference path="../decorators/autobind.ts" />
-///<reference path="../models/project.ts" />
-///<reference path="../models/drag-drop.ts" />
-namespace App {
-    // Project Item class
-    export class ProjectItem
-        extends Component<HTMLUListElement, HTMLLIElement>
-        implements Draggable
-    {
-        private project: Project;
+import { Draggable } from '../models/drag-drop.js';
+import { Component } from '../components/base-component.js';
+import { Project } from '../models/project.js';
+import { Autobind } from '../decorators/autobind.js';
 
-        get persons() {
-            if (this.project.people === 1) {
-                return '1 person';
-            } else {
-                return `${this.project.people} people`;
-            }
+// Project Item class
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+    private project: Project;
+
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        } else {
+            return `${this.project.people} people`;
         }
+    }
 
-        constructor(hostId: string, project: Project) {
-            super('single-project', hostId, false, project.id);
-            this.project = project;
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
 
-            this.configure();
-            this.renderContent();
-        }
+        this.configure();
+        this.renderContent();
+    }
 
-        @Autobind
-        dragStartHandler(event: DragEvent): void {
-            event.dataTransfer!.setData('text/plain', this.project.id);
-            event.dataTransfer!.effectAllowed = 'move';
-        }
+    @Autobind
+    dragStartHandler(event: DragEvent): void {
+        event.dataTransfer!.setData('text/plain', this.project.id);
+        event.dataTransfer!.effectAllowed = 'move';
+    }
 
-        dragEndHandler(_: DragEvent): void {
-            console.log('dragEnd');
-        }
+    dragEndHandler(_: DragEvent): void {
+        console.log('dragEnd');
+    }
 
-        configure(): void {
-            this.element.addEventListener('dragstart', this.dragStartHandler);
-            this.element.addEventListener('dragend', this.dragEndHandler);
-        }
+    configure(): void {
+        this.element.addEventListener('dragstart', this.dragStartHandler);
+        this.element.addEventListener('dragend', this.dragEndHandler);
+    }
 
-        renderContent(): void {
-            this.element.querySelector('h2')!.textContent = this.project.title;
-            this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
-            this.element.querySelector('p')!.textContent = this.project.description;
-        }
+    renderContent(): void {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
+        this.element.querySelector('p')!.textContent = this.project.description;
     }
 }
